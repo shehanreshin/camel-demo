1ab0873d7d97f06e2f26848590a7e70e812d6866
shehanreshin <asp.shehanreshin@gmail.com>
2024-07-14 17:36:35 +0530

    Initialize project

---
+++ src/main/java/edu/personal/cameldemo/CameldemoApplication.java
@@ -0,0 +1,13 @@
+package edu.personal.cameldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CameldemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CameldemoApplication.class, args);
+	}
+
+}
